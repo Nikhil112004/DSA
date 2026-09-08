5bc3f5784c9f69f011cd62974f537f776fea4249
Nikhil Gupta <164629304+Nikhil112004@users.noreply.github.com>
2026-09-08 11:17:44 +0530

    Time: 3 ms (88.12%) | Memory: 62.2 MB (46.14%) - LeetSync

--- 135-candy/candy.js
+++ 135-candy/candy.js
@@ -2,24 +2,28 @@
  * @param {number[]} ratings
  * @return {number}
  */
-var candy = function(ratings) {
-    let n = ratings.length;
-    let ltr = Array(n).fill(1);
-    for(let i = 1; i < n; i++){
-        if(ratings[i] > ratings[i - 1]){
-            ltr[i] = ltr[i - 1] + 1;
+var candy = function(arr) {
+    let n = arr.length;
+    let ans = n;
+    let i = 1;
+    while(i < n){
+        if(arr[i] === arr[i-1]){
+            ++i;
+            continue;
         }
-    }
-    let rtl = Array(n).fill(1);
-    for(let i = n-2; i >= 0; i--){
-        if(ratings[i] > ratings[i + 1]){
-            rtl[i] = rtl[i + 1] + 1;
+        let up = 0;
+        while(i < n && arr[i] > arr[i - 1]){
+            ++up;
+            ans += up;
+            ++i;
         }
-    }
-
-    let ans = 0;
-    for(let i = 0; i < n; i++){
-        ans = ans + Math.max(ltr[i], rtl[i]);
+        let down = 0;
+        while(i < n && arr[i] < arr[i - 1]){
+            ++down
+            ans += down;
+            ++i;
+        }
+        ans = ans - Math.min(up, down);
     }
     return ans;
 };
